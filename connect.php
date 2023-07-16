6010f21f473c698558ca16a47a1e02e6d1eb3f2a
Luciana <luchiswifter@gmail.com>
2023-07-16 15:50:44 -0300

    changee

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$email = $_POST['email'];
+$numberOfTickets = $_POST['numberOfTickets'];
+$category = $_POST['category'];
+
+//Database connection
+$conn new mysqli('localhost', 'root', '', 'formulario');
+if ($conn-> connect_error){
+    die('Connection Failed: '-> .$conn->connect_error);
+}else {
+    $stmt = $conn->prepare("insert into registration(firstName,lastName,email,numberOfTickets,category)
+    values(?,?,?,?,?)");
+    $stmt->bind_param("sssis", $firstName, $lastName, $email, $numberOfTickets, $category);
+    $stmt->execute();
+    echo "registration successfully";
+    $stmt->close();
+    $conn->close();
+}
+?>
